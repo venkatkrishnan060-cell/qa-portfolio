ac0069f6dc00d458dff90290fd63a8cd9af79135
venkatkrishnan060-cell <venkatkrishnan060@gmail.com>
2026-07-14 14:18:49 +0530

    Initial portfolio commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+var typed = new Typed("#typing", {
+
+    strings: [
+
+        "QA Engineer",
+
+        "Automation Tester",
+
+        "Selenium Engineer",
+
+        "Playwright Engineer",
+
+        "API Tester"
+
+    ],
+
+    typeSpeed:70,
+
+    backSpeed:50,
+
+    backDelay:1500,
+
+    loop:true
+
+});
